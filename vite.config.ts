a8565b430bd592f8f813e57e51fec095c119dc26
Ubuntu <ubuntu@localhost.localdomain>
2026-07-07 23:28:50 +0800

    fix: naikkan PWA maxFileSize ke 5MB untuk bundle 30 juz Sunda

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig(({mode}) => {
           ]
         },
         workbox: {
+          maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
           globPatterns: ['**/*.{js,css,html,ico,png,svg,mp3,woff2}'],
           runtimeCaching: [
             {
